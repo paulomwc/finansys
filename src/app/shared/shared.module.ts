2d0a953659a3ca6211fb8e92f0f20ace6fdacceb
Paulo Marcelo <paulomwalraven@gmail.com>
2019-05-27 15:50:22 -0300

    bread-crumb implementado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { RouterModule } from '@angular/router';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-  declarations: [BreadCrumbComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  declarations: [BreadCrumbComponent, PageHeaderComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [CommonModule, ReactiveFormsModule, BreadCrumbComponent]
 })
 export class SharedModule { }
